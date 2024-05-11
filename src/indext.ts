1e8199b69f352faafa1f5569596a16fdd30bd66f
sudo <meshackmasam@gmail.com>
2024-05-11 13:42:23 +0300

    Initial commit

---
+++ src/indext.ts
@@ -0,0 +1,70 @@
+export function parseArgs() {
+  //@ts-ignore
+  const args = process.argv.slice(2);
+  const items: {
+    name: string;
+    type: "$" | "-" | "--";
+    value: string | boolean;
+  }[] = [];
+  for (let i = 0; i < args.length; i++) {
+    const a = args[i];
+    if (a.slice(0, 2) == "--") {
+      if (a.indexOf("=") > -1) {
+        const ats = a.slice(2).split("=");
+        items.push({
+          type: "--",
+          name: ats[0],
+          value: ats[1],
+        });
+      } else {
+        const name = a.slice(2);
+        const n = args[i + 1];
+        let value: any = true;
+        if (n != undefined && n.slice(0, 1) != "-") {
+          value = n;
+          i++;
+        }
+        items.push({
+          type: "--",
+          name: name,
+          value: value,
+        });
+      }
+    } else if (a.slice(0, 1) == "-") {
+      if (a.indexOf("=") > -1) {
+        const ats = a.slice(1).split("=");
+        items.push({
+          type: "-",
+          name: ats[0],
+          value: ats[1],
+        });
+      } else {
+        const name = a.slice(1);
+        const n = args[i + 1];
+        let value: any = true;
+        if (n != undefined && n.slice(0, 1) != "-") {
+          value = n;
+          i++;
+        }
+        items.push({
+          type: "-",
+          name: name,
+          value: value,
+        });
+      }
+    } else {
+      items.push({
+        type: "$",
+        name: a,
+        value: true,
+      });
+    }
+  }
+  return items.reduce(
+    (o, item) => ({
+      ...o,
+      [item.name]: item.value,
+    }),
+    {},
+  );
+}
